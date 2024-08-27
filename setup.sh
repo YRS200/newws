937c6a812cab7ec736af304f3400b7bd9dd06f2a
YRS200 <110148911+YRS200@users.noreply.github.com>
2024-08-28 06:00:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -135,7 +135,7 @@ clear
 #install Xray
 echo -e "\e[0;32mINSTALLING XRAY CORE...\e[0m"
 sleep 1
-wget https://raw.githubusercontent.com/${GitUser}/ws/main/install/ins-xray.sh && chmod +x ins-xray.sh && screen -S ins-xray ./ins-xray.sh
+wget https://raw.githubusercontent.com/${GitUser}/newws/main/install/ins-xray.sh && chmod +x ins-xray.sh && screen -S ins-xray ./ins-xray.sh
 echo -e "\e[0;32mDONE INSTALLING XRAY CORE\e[0m"
 clear
 #install Trojan GO
